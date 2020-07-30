796db83fe20dddd207383600eec016dd405dfdbf
Birjot Bala <birjot.bala@gmail.com>
2020-07-30 14:44:48 -0400

    Refactored code

--- poe_helper/gui.py
+++ poe_helper/gui.py
@@ -19,21 +19,39 @@ class Gui(ttk.Frame):
         ttk.Frame.__init__(self, parent, padding=10)
         self.grid(column=0, row=0)
         self.grid_columnconfigure(0, weight=1)
-        self.createWidgets()
+        self.create_initial_widgets()
 
 
-    def createWidgets(self):
+    def create_initial_widgets(self):
         """Initial widge creation."""
-        self.search_button = ttk.Button(self, text="Search", command=self.output_results)
+        self.search_button = ttk.Button(self, text="Search", command=self.display_listings)
         self.price_label_frame = ttk.LabelFrame(self, text="")
         self.search_button.grid(column=0, row=1, pady=10, sticky="s")
         self.price_label_frame.grid(column=0, row=0, padx=10)
 
     
-    def output_results(self):
+    def create_listing_labels(self, resp):
+        """Create labels for listings."""
+        for counter, item in enumerate(resp):
+            self.price_title_label = ttk.Label(self.price_label_frame, text=f'{counter+1}:')
+            self.price_result_label = ttk.Label(self.price_label_frame, text=item.format_price())
+            self.price_result_label.grid(column=1, row=counter+10, sticky="e", padx=10, pady=2)
+            self.price_title_label.grid(column=0, row=counter+10, padx=10)
+
+
+    def display_item_image(self, resp):
+        """Displays the item image from the URL."""
+        image_url = resp[0].item_icon()
+        photo = image_from_url(image_url)
+        self.item_image_label = ttk.Label(self.price_label_frame, image=photo)
+        self.item_image_label.image = photo
+        self.item_image_label.grid(column=0, row=0, columnspan=2, pady=5)
+
+
+    def display_listings(self):
         """Retrieves and processes copied text from the clipboard.
 
-        Function creates labels of the listings retrieved and shows 
+        Creates labels of the listings retrieved and shows 
         an image of the item. If the copied text is not compatible
         displays an error message.
 
@@ -50,20 +68,11 @@ class Gui(ttk.Frame):
             item_dict = format_search_query(parsed_item["name"], parsed_item["type"])
             trade_response = search_trade_api(item_dict, poe_trade_api)
 
-            for counter, item in enumerate(trade_response):
-                self.price_title_label = ttk.Label(self.price_label_frame, text=f'{counter+1}:')
-                self.price_result_label = ttk.Label(self.price_label_frame, text=item.format_price())
-                self.price_result_label.grid(column=1, row=counter+10, sticky="e", padx=10, pady=2)
-                self.price_title_label.grid(column=0, row=counter+10, padx=10)
-
+            self.create_listing_labels(trade_response)
+            self.display_item_image(trade_response)
             self.price_label_frame.config(text=f'{parsed_item["name"]}\n{parsed_item["type"]}')
-            image_url = trade_response[0].item_icon()
-            photo = image_from_url(image_url)
             self.listing_label = ttk.Label(self.price_label_frame, text="Listings:")
             self.listing_label.grid(column=0, row=1)
-            self.item_image_label = ttk.Label(self.price_label_frame, image=photo)
-            self.item_image_label.image = photo
-            self.item_image_label.grid(column=0, row=0, columnspan=2, pady=5)
     
         except:
             self.except_label = ttk.Label(self.price_label_frame, text="Incorrect format of text in clipboard.")
