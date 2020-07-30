2af73a42bc2dbd32b0a8321823785d762712d3cb
Birjot Bala <birjot.bala@gmail.com>
2020-07-30 17:33:31 -0400

    Added entry point to setup.py

--- poe_helper/__main__.py
+++ poe_helper/__main__.py
@@ -2,12 +2,20 @@
 
 from gui import Gui
 
+def build_gui():
+    gui = Gui()
 
-gui = Gui()
+    gui.master.title('POE Helper')
+    gui.master.resizable(False,False)
+    gui.master.minsize(200,100)
+    gui.master.grid_columnconfigure(0, weight=1)
 
-gui.master.title('POE Helper')
-gui.master.resizable(False,False)
-gui.master.minsize(200,100)
-gui.master.grid_columnconfigure(0, weight=1)
+    gui.mainloop()
 
-gui.mainloop()
+
+def main(*args):
+    build_gui()
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -10,12 +10,17 @@ setup(
     name="poe_helper",
     version="0.1.dev",
     author="Birjot Bala",
-    description="Look up items on POE Trade API",
+    description="Look up items on the PoE Trade API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Birjot-Bala/poe-helper",
     packages=["poe_helper"],
     install_requires=requirements,
+    entry_points={
+        "gui_scripts": [
+            "poe_helper = poe_helper.__main__:main"
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
